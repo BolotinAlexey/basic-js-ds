ac2300f58ce608223fbcba613986c5a81a6519f2
Oleksii Bolotin <samkiral73@gmail.com>
2023-12-24 00:27:20 +0100

    feat: BinarySearchTree is done

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -13,6 +13,7 @@ class BinarySearchTree {
   }
 
   add(v) {
+    if (typeof v !== "number") return;
     if (!this.rootNode) {
       this.rootNode = new Node(v);
       return;
@@ -41,6 +42,7 @@ class BinarySearchTree {
   }
 
   has(v, isHas = true) {
+    if (typeof v !== "number") return false;
     if (!this.rootNode) return isHas ? false : null;
     let node = this.rootNode;
     while (true) {
@@ -60,19 +62,54 @@ class BinarySearchTree {
     return this.has(v, false);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  remove(v, node = this.rootNode, parent) {
+    if (typeof v !== "number") return;
+    if (!this.rootNode || !node) return;
+    if (node.data > v) this.remove(v, node.left, node);
+    if (node.data < v) this.remove(v, node.right, node);
+
+    if (node.data !== v) return;
+    if (!node.left) {
+      if (!parent) this.rootNode = node.right;
+      else {
+        if (parent.left === node) parent.left = node.right;
+        else parent.right = node.right;
+      }
+      return;
+    }
+    if (!node.right) {
+      if (!parent) this.rootNode = node.left;
+      else {
+        if (parent.left === node) parent.left = node.left;
+        else parent.right = node.left;
+      }
+      return;
+    }
+    let minNode = node.right;
+    let prevNode = node.right;
+    while (minNode.left) {
+      prevNode = minNode;
+      minNode = minNode.left;
+    }
+    prevNode.left = null;
+    if (node === this.rootNode) this.rootNode = minNode;
+    // const rmNode = node;
+    minNode.left = node.left;
+    node = minNode;
+    if (node.right) {
+      this.max(node, false).right = prevNode;
+    }
   }
 
+  // min max
   min(node = this.rootNode) {
     if (node.left) return this.min(node.left);
     return node.data;
   }
 
-  max(node = this.rootNode) {
-    if (node.right) return this.max(node.right);
-    return node.data;
+  max(node = this.rootNode, isValue = true) {
+    if (node.right) return this.max(node.right, isValue);
+    return isValue ? node.data : node;
   }
 }
 
